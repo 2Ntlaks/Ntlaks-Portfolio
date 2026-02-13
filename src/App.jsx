3c9833685194a8a8280c19c8f9e42cbbc95866fa
Ntlakanipho Mgaguli <ntlakaniphomgaguli210@gmail.com>
2026-02-13 19:40:54 +0200

    refactor(ui): remove proof section and revert proof-only navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import ProofBar from "./components/ProofBar";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
@@ -13,7 +12,6 @@ function App() {
       <Navbar />
       <main>
         <Hero />
-        <ProofBar />
         <About />
         <Projects />
         <Contact />
--- src/constants/publicStats.js
+++ src/constants/publicStats.js
@@ -2,7 +2,4 @@ export const PUBLIC_STATS = {
   udemyLearners: 121,
   udemyPublicReviews: 9,
   udemyLectures: 37,
-  tiktokFollowers: 333,
-  tiktokLikes: 751,
-  studyMaterialYears: "2022-2024",
 };
